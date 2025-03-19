1d12892345111fc191cc9166d648a18ea07e646d
gabii23s <danielmascarenhas2007@gmail.com>
2025-03-19 19:50:35 +0000

    exercicio

--- 1. tipos de dados/2 estrutura condicional/10_atividade.py
+++ 1. tipos de dados/2 estrutura condicional/10_atividade.py
@@ -9,7 +9,7 @@ match sexo:
     case  "M":
         peso_ideal = (72.7 * altura) -58
         print(f"\npeso ideal: {peso_ideal}")
-    case "F":
+    case  "F":
         peso_ideal = (62.1 * altura) -44.7
         print(f"\npeso ideal: {peso_ideal}")
  
